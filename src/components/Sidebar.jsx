adf945b92666daab503ba6d1f90fecba86b9a36b
Colli Gregory <colli.gregory.13@gmail.com>
2022-02-25 13:51:46 +0100

    modif style nav mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 
 export const Sidebar = () => {
   const user = useSelector((state) => state.userReducer);
 
 
   return (
+    // <div>
+    //   <div className="sideBar">
+    //     <div className="sideBar-logo">
+    //       <img src="img/logo_groupomania.png" alt="logo groupomania" />
+    //     </div>
+        // {user.isLog ? (
+        //   <div className="sideBar-user row">
+        //     <img src={user.image} alt="logo utilisateur" className="col s9 circle responsive-img" />
+        //     <div>{user.username}</div>
+        //     <div>{user.email}</div>
+        //   </div>
+        // ) : null}
+
+    //     <div activeclass="active" className="bloc-link">
+    //       <span className="nav-links">Créer un post</span>
+    //     </div>
+    //   </div>
+    // </div>
     <div>
-      <div className="sideBar">
-        <div className="sideBar-logo">
-          <img src="img/logo_groupomania.png" alt="logo groupomania" />
-        </div>
-        {user.isLog ? (
+
+      <nav>
+        <div className='nav-wrapper'>
+          <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <div className='sideBar hide-on-med-and-down'>
+
+            <div className='sideBar-logo'>
+            <img src="img/logo_groupomania.png" alt="logo groupomania" />
+            </div>
+
+            {user.isLog ? (
           <div className="sideBar-user row">
             <img src={user.image} alt="logo utilisateur" className="col s9 circle responsive-img" />
             <div>{user.username}</div>
@@ -19,10 +44,30 @@ export const Sidebar = () => {
           </div>
         ) : null}
 
-        <div activeclass="active" className="bloc-link">
-          <span className="nav-links">Créer un post</span>
+            <NavLink activeclass="active" to="/creerPost" className='bloc-link'>
+              <i className="fas fa-book-open"></i>
+              <span className='nav-links'>Créer un post</span>
+            </NavLink>
+          </div>
         </div>
-      </div>
+      </nav>
+
+
+      <ul className="sidenav sidenav-close" id="mobile-demo">
+      {user.isLog ? (
+          <div className="sideBar-user row">
+            <img src={user.image} alt="logo utilisateur" className="col s9 circle responsive-img" />
+            <div>{user.username}</div>
+            <div>{user.email}</div>
+          </div>
+        ) : null}
+      <NavLink activeclass="active" to="/creerPost" className='bloc-link'>
+              <i className="fas fa-book-open"></i>
+              <span className='nav-links'>Créer un post</span>
+            </NavLink>
+      </ul>
+
+
     </div>
   );
 };
