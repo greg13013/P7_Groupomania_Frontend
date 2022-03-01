79c6d25e262f502af812c62d813d676c9486158c
Colli Gregory <colli.gregory.13@gmail.com>
2022-03-01 15:40:57 +0100

    ajout centrage post

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const CardPost = ({ post }) => {
   return (
     <div className="cardPerso cardPost centrerContainer flexColumn">
-      <div className="card-header col s12">
+      <div className="card-header col s12 centrerContainer">
         <img src={post.image} alt={post.image} className="responsive-img" />
       </div>
       <div className="card-contenu">{post.contenu}</div>
