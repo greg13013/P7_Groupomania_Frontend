b9dd426e231100af052016ee73ae7a7c476153aa
Colli Gregory <colli.gregory.13@gmail.com>
2022-03-09 12:18:32 +0100

    essai deploiement netlify

--- src/services/config.js
+++ src/services/config.js
@@ -3,8 +3,16 @@ import cookie from 'js-cookie'
 
 
 
+// export default axios.create({
+//   baseURL: "http://localhost:3001",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
+
+//Config deploiement netlify
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://colligroupomania.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
